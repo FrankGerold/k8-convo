0662f602f5eb25ec745413e13cfea984c60b8c87
Frank Gerold <FrankETSGerold@gmail.com>
2020-05-28 14:53:50 -0400

    Deploymment script - git SHA tags for docker images, travis env vars

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t callipygious/multi-client:latest -t callipygious/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t callipygious/multi-server:latest -t callipygious/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t callipygious/multi-worker:latest -t callipygious/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push callipygious/multi-client:latest
+docker push callipygious/multi-worker:latest
+docker push callipygious/multi-server:latest
+docker push callipygious/multi-client:$SHA
+docker push callipygious/multi-worker:$SHA
+docker push callipygious/multi-server:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=callipygious/multi-server:$SHA
+kubectl set image deployments/client-deployment server=callipygious/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=callipygious/multi-worker:$SHA
